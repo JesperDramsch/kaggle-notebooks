143b745084d9c76f6df148d6d569709d0d1fb42a
Jesper Dramsch <jesper@dramsch.net>
2022-05-23 01:25:12 +0200

    Remove Kaggle API

--- utils/create_readme.py
+++ utils/create_readme.py
@@ -1,7 +1,3 @@
-from kaggle.api.kaggle_api_extended import KaggleApi
-api = KaggleApi()
-api.authenticate()
-
 # slug: [Description, Competition or Freestyle]
 competition = {"intro-to-seismic-salt-and-how-to-geophysics": "Geophysics competition to identify salt bodies in seismic data. The competition was hosted by the seismic contractor TGS and provided patched seismic images with varying amounts of noise. The masks were provided as pixel-encoded masks to save bandwidth.",
                 "intro-chest-xray-dicom-viz-u-nets-full-data": "Medical competition to identify pneumothorax in chest X-rays. Data provided in medical DICOM format. Very large images that benefit from downsampling.",
@@ -9,15 +5,26 @@ competition = {"intro-to-seismic-salt-and-how-to-geophysics": "Geophysics compet
                 "intro-to-connextx-env-and-minimax": "First reinforcement learning playground using Connect 4 on Kaggle to test new environment. Agents play against each other on the leaderboard. ConnectX is usually best approach by the Negamax algorithm, a Minimax variant.",
                 "intro-to-deep-fakes-videos-and-metadata-eda": "Deep Fake competition to identify visual and sound manipulation usually using GANs in video footage.",
                 "getting-started-with-standard-gans-tutorial": "Least Squares GAN on TPU to generate Monet painting from noise. Uses data augmentation to enrich the training set.",
-                "understanding-and-improving-cyclegans-tutorial": "CycleGAN on TPUs to generate Monet painting from photographs. Uses Least Squares implementation of GANs to improve training and also implements some basic augmentation to get the competition going.",}
+                "understanding-and-improving-cyclegans-tutorial": "CycleGAN on TPUs to generate Monet painting from photographs. Uses Least Squares implementation of GANs to improve training and also implements some basic augmentation to get the competition going.",
+                "introduction-to-volcanology-seismograms-and-lgbm": "Volcano monitoring is important for both the inhabitants on and next to volcanoes, but also globally, as seen with the Eyjafjallajökull eruption disrupting air travel ten years ago. The notebook is an introduction to volcanology with a small LightGBM model as a starter model."}
 
 # slug: [Description, Competition or Freestyle]
 freestyle = {"the-reason-we-re-happy": "Exploration of the 'World Happiness Report' data. Found a strong correlation of final score with wealth indicators. June winner in Reddit community [r/dataisbeautiful](https://www.reddit.com/r/dataisbeautiful/comments/c89mz2/battle_dataviz_battle_for_the_month_of_july_2019/eskzdhd/).",
             "berlin-airbnbs-is-it-really-all-about-location": "AirBnB has been a source of income for many with a spare room, but become controversial due to commercial players buying apartments to rent out on the platform. In this analysis, we are looking at AirBnB data in Berlin, Germany. The questions are, what is the main influence on price of a rental. These questions are important for tourism, city planning, and renters alike. Where do affluent tourists rent AirBnBs? What decides a good price of your flat. Where can city planners accomodate for this new development of short term rental on the market?",}
 
 ## Sad times
-correction = {"getting-started-with-standard-gans-tutorial": -1,
-              "understanding-and-improving-cyclegans-tutorial": -1,}
+correction = {"intro-to-connextx-env-and-minimax": -1,}
+
+total_votes = "intro-to-seismic-salt-and-how-to-geophysics": 784,
+              "intro-chest-xray-dicom-viz-u-nets-full-data": 450,
+              "intro-to-santa-s-2019-viz-costs-22-s-and-search": 39,
+              "intro-to-connextx-env-and-minimax": 56,
+              "intro-to-deep-fakes-videos-and-metadata-eda": 39,
+              "getting-started-with-standard-gans-tutorial": 28,
+              "understanding-and-improving-cyclegans-tutorial": 34,
+              "the-reason-we-re-happy": 141,
+              "berlin-airbnbs-is-it-really-all-about-location": 15,
+              "introduction-to-volcanology-seismograms-and-lgbm": 80,}
 
 medal = lambda num, corr: ["", "🥉", "🥈", "🥇"][(num >= 5) + (num >= 20) + (num >= 50) + corr]
 
@@ -39,15 +46,13 @@ badge_template = ["[![](https://img.shields.io/badge/view-notebook-orange)](note
 
 
 for name, info in competition.items():
-    kernel = api.kernels_list(search=name)[0]
     badges = " ".join(badge_template).format(nb_type="competiton", name=name)
     string += f"### {medal(kernel.totalVotes, correction.get(name, 0))} {kernel.title} \n {badges}\n\n{info}\n\n"
 
 string += "## Free-style Notebooks\n"
 for name, info in freestyle.items():
-    kernel = api.kernels_list(search=name)[0]
     badges = " ".join(badge_template).format(nb_type="freestyle", name=name)
-    string += f"### {medal(kernel.totalVotes, correction.get(name, 0))} {kernel.title} \n {badges}\n\n{info}\n\n"
+    string += f"### {medal(total_votes.get(name, 0), correction.get(name, 0))} {name.replace("-", " ").title()} \n {badges}\n\n{info}\n\n"
 
 with open("README.md", "w", encoding="utf-8") as f:
     f.write(string)
